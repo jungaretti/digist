0a5871430b0a239754c128446a7238b239c28b1c
JP Ungaretti <t-jaunga@microsoft.com>
2020-07-29 15:09:38 -0500

    Add pugtester

---
+++ pugtester.js
@@ -0,0 +1,21 @@
+const pug = require('pug');
+
+// Compile the source code
+const compiledFunction = pug.compileFile('views/snippet.pug');
+
+// Render a set of data
+console.log(compiledFunction({
+    slice: {
+        name: 'hello_world.rb'
+    },
+    files: [{
+        name: 'hello_world.rb',
+        content: 'class HelloWorld\n   def initialize(name)\n      @name = name.capitalize\n   end\n   def sayHi\n      puts \"Hello !\"\n   end\nend\n\nhello = HelloWorld.new(\"World\")\nhello.sayHi',
+        iconURL: 'https://raw.githubusercontent.com/jesseweed/seti-ui/master/icons/ruby.svg'
+    }, {
+        name: 'hello_world.py',
+        content: 'class HelloWorld:\n\n    def __init__(self, name):\n        self.name = name.capitalize()\n       \n    def sayHi(self):\n        print \"Hello \" + self.name + \"!\"\n\nhello = HelloWorld(\"world\")\nhello.sayHi()',
+        iconURL: 'https://raw.githubusercontent.com/jesseweed/seti-ui/master/icons/ruby.svg'
+    }]
+  }));
+  
