4133061db8452bf8df4a71ce7ccb574ad31336f1
Kartik <KartMahajan@gmail.com>
2020-07-31 11:50:12 -0700

    readability fix

--- routes/gistProvider.js
+++ routes/gistProvider.js
@@ -87,7 +87,7 @@ function checkFileExists(file, gistInfo){
 
 function checkSliceInFile(file, gistInfo, start, stop){
     let totalLines = gistInfo[file]["content"].split("\n").length;
-    return ((totalLines >= start) && (totalLines >= stop));
+    return ((start <= totalLines) && (stop <= totalLines));
 }
 
 function composeGistUrl(userId, gistId) {
